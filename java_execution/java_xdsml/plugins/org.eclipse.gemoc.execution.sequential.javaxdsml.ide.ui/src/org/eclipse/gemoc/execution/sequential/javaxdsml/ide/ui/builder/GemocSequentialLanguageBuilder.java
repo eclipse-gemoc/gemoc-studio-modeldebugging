7746487e5e0e65ea04525ad84eee28aae34db0d1
Fabien Coulon <fabien.coulon@inria.fr>
2017-10-02 14:16:28 +0200

    Fix IndexOutOfBoundsException in the Builder when creating .dsl file

--- java_execution/java_xdsml/plugins/org.eclipse.gemoc.execution.sequential.javaxdsml.ide.ui/src/org/eclipse/gemoc/execution/sequential/javaxdsml/ide/ui/builder/GemocSequentialLanguageBuilder.java
+++ java_execution/java_xdsml/plugins/org.eclipse.gemoc.execution.sequential.javaxdsml.ide.ui/src/org/eclipse/gemoc/execution/sequential/javaxdsml/ide/ui/builder/GemocSequentialLanguageBuilder.java
@@ -112,12 +112,15 @@ public class GemocSequentialLanguageBuilder extends IncrementalProjectBuilder {
 			if (file.exists()) {
 				
 				Resource res = (new ResourceSetImpl()).getResource(URI.createURI(file.getFullPath().toOSString()), true);
-				Dsl dsl = (Dsl) res.getContents().get(0);
-				String languageName = dsl.getName();
-				if(languageName == null || languageName.isEmpty()) {
-					languageName = file.getName();
+				if(res != null && res.getContents().size() > 0 && res.getContents().get(0) instanceof Dsl) {
+					Dsl dsl = (Dsl) res.getContents().get(0);
+					String languageName = dsl.getName();
+					if(languageName == null || languageName.isEmpty()) {
+						languageName = file.getName();
+					}
+					setPluginLanguageNameAndFilePath(project, file, languageName);
+					
 				}
-				setPluginLanguageNameAndFilePath(project, file, languageName);
 				
 				//Use default model loader
 				updateModelLoaderClass(project, null);
