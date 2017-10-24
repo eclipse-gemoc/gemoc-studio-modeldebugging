b795451a49f7a09b5a3e7dbf7c9a6a443a1694fd
dvojtise <didier.vojtisek@inria.fr>
2017-10-24 17:19:48 +0200

    added an helper class to expose IDs of the UI
    Signed-off-by: dvojtise <didier.vojtisek@inria.fr>

---
+++ framework/xdsml_framework/plugins/org.eclipse.gemoc.xdsmlframework.ide.ui/src/org/eclipse/gemoc/xdsmlframework/ide/ui/XDSMLFrameworkUI.java
@@ -0,0 +1,20 @@
+package org.eclipse.gemoc.xdsmlframework.ide.ui;
+
+/**
+ * Central access point for the XDSML framework ide UI plug-in (id <code>"org.eclipse.gemoc.xdsmlframework.ide.ui"</code>).
+
+ * This class provides static methods and fields only; it is not intended to be
+ * instantiated or subclassed by clients.
+ * </p>
+ *
+ * @noinstantiate This class is not intended to be instantiated by clients.
+ */
+
+public class XDSMLFrameworkUI {
+
+	/**
+	 * The id of the XDSML perspective
+	 * (value <code>"org.eclipse.gemoc.xdsmlframework.ide.ui"</code>).
+	 */
+	public static final String ID_PERSPECTIVE= 		"org.eclipse.gemoc.xdsmlframework.ide.ui.xdsml.perspective"; //$NON-NLS-1$
+}
