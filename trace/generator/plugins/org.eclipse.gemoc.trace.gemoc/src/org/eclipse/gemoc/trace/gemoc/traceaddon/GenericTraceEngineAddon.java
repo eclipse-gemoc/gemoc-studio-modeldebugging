2a2bc693e054b78eaf46d678eb0ed2d0677efbe4
Fabien Coulon <fabien.coulon@obeo.fr>
2018-05-28 16:21:58 +0200

    [Trace Generator] Add default implementation for IDynamicPartAccessor

--- trace/generator/plugins/org.eclipse.gemoc.trace.gemoc/src/org/eclipse/gemoc/trace/gemoc/traceaddon/GenericTraceEngineAddon.java
+++ trace/generator/plugins/org.eclipse.gemoc.trace.gemoc/src/org/eclipse/gemoc/trace/gemoc/traceaddon/GenericTraceEngineAddon.java
@@ -14,6 +14,7 @@ import java.util.Map;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.gemoc.executionframework.debugger.DefaultDynamicPartAccessor;
 import org.eclipse.gemoc.executionframework.debugger.IDynamicPartAccessor;
 import org.eclipse.gemoc.executionframework.debugger.IMutableFieldExtractor;
 import org.eclipse.gemoc.trace.commons.model.trace.State;
@@ -26,7 +27,7 @@ import org.eclipse.gemoc.trace.gemoc.api.ITraceConstructor;
 public class GenericTraceEngineAddon extends AbstractTraceAddon {
 
 	private GenericTraceStepFactory factory = null;
-	IDynamicPartAccessor dynamicPartAccessor;
+	IDynamicPartAccessor dynamicPartAccessor = new DefaultDynamicPartAccessor();
 	
 	@Override
 	public IStepFactory getFactory() {
