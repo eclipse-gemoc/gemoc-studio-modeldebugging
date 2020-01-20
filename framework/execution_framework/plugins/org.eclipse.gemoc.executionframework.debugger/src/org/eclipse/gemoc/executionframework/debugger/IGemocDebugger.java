3fe2a4753908c5712de2e4171c7837485fddc884
Didier Vojtisek <didier.vojtisek@inria.fr>
2020-01-20 17:23:42 +0100

    add complianceLevel to java 1.8 that xtend compiler understands
    Signed-off-by: Didier Vojtisek <didier.vojtisek@inria.fr>

--- framework/execution_framework/plugins/org.eclipse.gemoc.executionframework.debugger/src/org/eclipse/gemoc/executionframework/debugger/IGemocDebugger.java
+++ framework/execution_framework/plugins/org.eclipse.gemoc.executionframework.debugger/src/org/eclipse/gemoc/executionframework/debugger/IGemocDebugger.java
@@ -10,6 +10,8 @@
  *******************************************************************************/
 package org.eclipse.gemoc.executionframework.debugger;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.BiPredicate;
 
 import org.eclipse.gemoc.trace.commons.model.trace.Step;
@@ -22,5 +24,11 @@ public interface IGemocDebugger extends IEngineAddon {
 
 	public abstract void addPredicateBreakpoint(BiPredicate<IExecutionEngine<?>, Step<?>> predicate);
 
+	@Override
+	default List<String> getTags() {
+		// add the "GemocDebugger" tag to the list
+		return  Arrays.asList(GROUP_TAG, getAddonID());
+	}
+	
 	public final String GROUP_TAG = "GemocDebugger";
 }
