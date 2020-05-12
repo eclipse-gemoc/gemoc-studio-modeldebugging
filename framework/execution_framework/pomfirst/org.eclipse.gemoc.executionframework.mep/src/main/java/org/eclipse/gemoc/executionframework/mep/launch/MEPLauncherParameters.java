143213871a778733238937ad1805195ac3609b7c
Pierre Jeanjean <pierre.jeanjean@inria.fr>
2020-05-12 15:40:46 +0200

    improves MEP services
    implements breakpoints
    
    Signed-off-by: Pierre Jeanjean <pierre.jeanjean@inria.fr>

---
+++ framework/execution_framework/pomfirst/org.eclipse.gemoc.executionframework.mep/src/main/java/org/eclipse/gemoc/executionframework/mep/launch/MEPLauncherParameters.java
@@ -0,0 +1,13 @@
+package org.eclipse.gemoc.executionframework.mep.launch;
+
+import org.eclipse.emf.ecore.resource.Resource;
+
+public class MEPLauncherParameters {
+	
+	public Resource resourceModel = null;
+	public String modelEntryPoint = "";
+	public String methodEntryPoint = "";
+	public String initializationMethod = "";
+	public String initializationMethodArgs = "";
+
+}
