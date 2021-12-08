4293c3d201f371f1c5dd0786a120b0e9e912134e
Didier Vojtisek <didier.vojtisek@inria.fr>
2021-12-08 18:11:53 +0100

    add ws.server plugin
    Signed-off-by: Didier Vojtisek <didier.vojtisek@inria.fr>

---
+++ framework/execution_framework/plugins/org.eclipse.gemoc.executionframework.addon.eaop.server/src/org/eclipse/gemoc/executionframework/addon/eaop/server/EaopEngineAddon.java
@@ -0,0 +1,11 @@
+package org.eclipse.gemoc.executionframework.addon.eaop.server;
+
+import org.eclipse.gemoc.xdsmlframework.api.engine_addon.IEngineAddon;
+
+public class EaopEngineAddon implements IEngineAddon {
+
+	public EaopEngineAddon() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
