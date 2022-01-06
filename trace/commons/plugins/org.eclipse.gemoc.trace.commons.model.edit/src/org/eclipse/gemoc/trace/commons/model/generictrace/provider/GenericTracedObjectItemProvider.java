9e200210a7e9bc44202530586164e5d8a95ced7e
Erwan Bousse <erwan.bousse@ls2n.fr>
2022-01-06 16:54:38 +0100

    Set '@generated NOT' on modified getChildrenFeatures method

--- trace/commons/plugins/org.eclipse.gemoc.trace.commons.model.edit/src/org/eclipse/gemoc/trace/commons/model/generictrace/provider/GenericTracedObjectItemProvider.java
+++ trace/commons/plugins/org.eclipse.gemoc.trace.commons.model.edit/src/org/eclipse/gemoc/trace/commons/model/generictrace/provider/GenericTracedObjectItemProvider.java
@@ -79,14 +79,14 @@ public class GenericTracedObjectItemProvider extends TracedObjectItemProvider {
 				 null,
 				 null));
 	}
-
+ 
 	/**
 	 * This specifies how to implement {@link #getChildren} and is used to deduce an appropriate feature for an
 	 * {@link org.eclipse.emf.edit.command.AddCommand}, {@link org.eclipse.emf.edit.command.RemoveCommand} or
 	 * {@link org.eclipse.emf.edit.command.MoveCommand} in {@link #createCommand}.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * @generated NOT
 	 */
 	@Override
 	public Collection<? extends EStructuralFeature> getChildrenFeatures(Object object) {
