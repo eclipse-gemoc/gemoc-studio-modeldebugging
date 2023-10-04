82a8fce661189d42d41c48bd6a493295a8940c20
Erwan Bousse <erwan.bousse@ls2n.fr>
2023-10-04 16:18:31 +0200

    Add more constructors to ConcurrentStepException

--- framework/execution_framework/plugins/org.eclipse.gemoc.executionframework.engine/src/org/eclipse/gemoc/executionframework/engine/concurrency/ConcurrentStepException.java
+++ framework/execution_framework/plugins/org.eclipse.gemoc.executionframework.engine/src/org/eclipse/gemoc/executionframework/engine/concurrency/ConcurrentStepException.java
@@ -2,8 +2,18 @@ package org.eclipse.gemoc.executionframework.engine.concurrency;
 
 public class ConcurrentStepException extends Exception {
 
+	private static final long serialVersionUID = 9140570714416166932L;
+
 	public ConcurrentStepException(Exception e) {
 		super(e);
+	}
+	
+	public ConcurrentStepException(String message, Exception cause) {
+		super(message, cause);
+	}
+	
+	public ConcurrentStepException(String message) {
+		super(message);
 	}
 
 }
